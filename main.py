35b5189d332bf7402988e03eab69e5f2dfa0d4b4
Nathan Lopez <contact@lopeznathan.com>
2019-05-14 15:13:24 -0500

    Implemented dnspython Record Lookup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 from flask import Flask
+from dns import resolver
+
+dnsResolver = resolver.Resolver()
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/api/v1/<record_type>/<domain>', methods=['GET'])
+def record_lookup(record_type, domain):
+    try:
+        response = dnsResolver.query(domain, record_type.upper())
+        return str([str(r) for r in response])
+    except Exception as e:
+        return str(e)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
