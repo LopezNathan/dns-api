15acf655e4fce5cc6dc9a23d42169dbafb0018a0
Nathan Lopez <contact@lopeznathan.com>
2019-05-15 00:56:35 -0500

    Flake8 Changes

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from dns import resolver
 dnsResolver = resolver.Resolver()
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 @app.route('/api/v1/<record_type>/<domain>', methods=['GET'])
 def record_lookup(record_type, domain):
@@ -16,6 +18,7 @@ def record_lookup(record_type, domain):
         return str([str(r) for r in response])
     except Exception as e:
         return str(e)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
